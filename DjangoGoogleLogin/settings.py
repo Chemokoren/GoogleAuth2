1b52214ad2071c1026ea114f47c2119120187dbf
Kibet Masai <humphreykibet@gmail.com>
2021-11-11 11:03:11 +0300

    updated settings for production hkm

--- DjangoGoogleLogin/settings.py
+++ DjangoGoogleLogin/settings.py
@@ -89,6 +89,11 @@ DATABASES = {
     }
 }
 
+# overrides the database settings to use postgreSQL
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+DATABASES['default']['CONN_MAX_AGE'] = 500
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
